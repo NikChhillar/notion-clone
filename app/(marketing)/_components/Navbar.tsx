5cb73f9574bb8e48fb36caf86df8ebeb899ebefc
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-10-13 12:13:15 +0530

    convex-clerk setup 🗒️

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,9 +4,16 @@ import { ModeToggle } from "@/components/ModeToggle";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useConvexAuth } from "convex/react";
+import Spinner from "@/components/Spinner";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
+
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
     <div
@@ -20,6 +27,28 @@ const Navbar = () => {
         <p className="font-semibold">Insightful</p>
       </div>
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Started</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Insightful</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: 'https://unique-dinosaur-98.clerk.accounts.dev',
+            applicationID: 'convex'
+        }
+    ]
+}
