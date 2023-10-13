a247570a2b6a2e26f5e5d20a5cd6f0e604001c30
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-10-13 12:42:59 +0530

    landing page completed ⭕

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -2,8 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useConvexAuth } from "convex/react";
+import Spinner from "@/components/Spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
@@ -14,10 +20,27 @@ const Heading = () => {
         Insightful: Your Gateway to Streamlined Productivity <br /> and Creative
         Excellence
       </h3>
-      <Button>
-        Get Started
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Insightful
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Started
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
