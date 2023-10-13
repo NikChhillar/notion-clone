7da6e3d0702c282c9a0493d9296c84cd4789502f
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-10-13 11:23:35 +0530

    Navbar 🗒️

--- app/(marketing)/_components/Footer.tsx
+++ app/(marketing)/_components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
       <div className="hidden md:flex items-center gap-x-2">
-        <Image src="/logo.png" height="30" width="30" alt="Logo" />
+        <Image src="/logo.png" height="25" width="25" alt="Logo" />
         <p className={cn("font-semibold", font.className)}>Insightful</p>
       </div>
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,9 +1,28 @@
-'use client'
+"use client";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+  const scrolled = useScrollTop();
 
-export default Navbar
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <div className="hidden md:flex items-center gap-x-2">
+        <Image src="/logo.png" height="25" width="25" alt="Logo" />
+        <p className="font-semibold">Insightful</p>
+      </div>
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        Login
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
