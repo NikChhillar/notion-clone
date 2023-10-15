a37ca8260a66adff03e35eec74d3f14c1d34490a
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-10-15 15:00:39 +0530

    added hook for mobile responsiveness

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -2,8 +2,21 @@
 
 import { cn } from "@/lib/utils";
 import { ChevronsLeft } from "lucide-react";
+import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 const Navigation = () => {
+  //
+
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  //
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
   return (
     <>
       <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
