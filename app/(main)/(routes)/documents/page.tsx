e64743ae30f8f8a70cc0ffbe080e625ca366da37
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-10-24 11:04:40 +0530

    document creation page 🗒️

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const DocumentsPage = () => {
-    return (
-        <div>DocumentsPage</div>
-    )
-}
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to Nikhil&apos;s Insightful Dashboard
+      </h2>
+      <Button onClick={() => {}}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
 
-export default DocumentsPage
+export default DocumentsPage;
