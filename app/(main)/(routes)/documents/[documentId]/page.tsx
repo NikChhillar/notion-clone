64f826fbe11946f3147c308a4f2837fedd3fd158
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-11-01 15:52:47 +0530

    Icon picker for documents

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -28,9 +28,9 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40">
-      DocumentIdPage
+      <div className="h-[35vh]"></div>
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-      <Toolbar initialData={document} />
+        <Toolbar initialData={document} />
       </div>
     </div>
   );
--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
 
-const Toolbar = () => {
-  return (
-    <div>Toolbar</div>
-  )
+import { Doc } from "@/convex/_generated/dataModel";
+import IconPicker from "./IconPicker";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+interface ToolbarProps {
+  initialData: Doc<"documents">;
+  preview?: boolean;
 }
 
-export default Toolbar
+const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const update = useMutation(api.documents.update);
+
+  const onIconSelect = (icon: string) => {
+    update({
+      id: initialData._id,
+      icon,
+    });
+  };
+
+  return (
+    <div className="pl-[54px] group relative">
+      {!!initialData.icon && !preview && (
+        <div className="flex items-center gap-x-2 group/icon pt-6">
+          <IconPicker onChange={onIconSelect}>
+            <p className="text-6xl hover:opacity-75 transition">
+              {initialData.icon}
+            </p>
+          </IconPicker>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Toolbar;
