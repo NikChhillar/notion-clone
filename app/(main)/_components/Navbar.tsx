d1775e4b9f2804a8330d13d2aa72fe540af09102
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-11-01 15:40:45 +0530

    document page 🗒️

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -49,6 +49,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <div>Publish</div>
             <Menu documentId={document._id} />
           </div>
         </div>
